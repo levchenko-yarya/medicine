ac9d5117ccaabe0199a26c6e0860bb7b343f4437
levchenko-yarya <levchenkoyarya@gmail.com>
2021-07-09 20:12:22 +0300

    migrate database

---
+++ database/migrations/2021_07_09_165652_create_active_substances_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActiveSubstancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('active_substances', function (Blueprint $table) {
+            $table->Increments('id')->index();
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('active_substances');
+    }
+}
---
+++ database/migrations/2021_07_09_170126_create_pharmacological_agents_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePharmacologicalAgentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pharmacological_agents', function (Blueprint $table) {
+            $table->Increments('id')->index();
+            $table->string('name');
+            $table->integer('active_substance_id')->nullable()->unsigned();
+            $table->foreign('active_substance_id', 'pharmacological_agents_active_substances_id_active_substance_id')
+                ->references('id')
+                ->on('active_substances')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('manufacturer_id')->nullable()->unsigned();
+            $table->foreign('manufacturer_id', 'pharmacological_agents_manufacturers_id_manufacturer_id')
+                ->references('id')
+                ->on('manufacturers')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pharmacological_agents');
+    }
+}
